f63ff9eb334a24de198396467aabe8eecd2cad50
an-rias01 <anlttse183723@fpt.edu.vn>
2024-10-05 16:33:26 +0700

    CartDTO, CartItems, PaymentDTO, ProductVariants

--- SMSystem/src/java/model/payment/PaymentDTO.java
+++ SMSystem/src/java/model/payment/PaymentDTO.java
@@ -10,5 +10,48 @@ package model.payment;
  * @author LENOVO
  */
 public class PaymentDTO {
+    private int paymentMethod;
+    private String paymentName;
+    private int totalPayment;
+
+    public int getPaymentMethod() {
+        return paymentMethod;
+    }
+
+    public void setPaymentMethod(int paymentMethod) {
+        this.paymentMethod = paymentMethod;
+    }
+
+    public String getPaymentName() {
+        return paymentName;
+    }
+
+    public void setPaymentName(String paymentName) {
+        this.paymentName = paymentName;
+    }
+
+    public int getTotalPayment() {
+        return totalPayment;
+    }
+
+    public void setTotalPayment(int totalPayment) {
+        this.totalPayment = totalPayment;
+    }
+
+    public PaymentDTO(int paymentMethod, String paymentName, int totalPayment) {
+        this.paymentMethod = paymentMethod;
+        this.paymentName = paymentName;
+        this.totalPayment = totalPayment;
+    }
+
+    public PaymentDTO() {
+        this.paymentMethod=0;
+        this.paymentName="";
+        this.totalPayment=0;
+    }
+
+ 
     
 }
+
+   
