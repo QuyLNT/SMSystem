1e0094dcf9b129bea38992f0805f662673279666
QuyLNT <quylntse184583@fpt.edu.vn>
2024-10-05 15:10:15 +0700

    UserObjectDTO

--- SMSystem/src/java/model/category/UserObjectDTO.java
+++ SMSystem/src/java/model/category/UserObjectDTO.java
@@ -10,5 +10,46 @@ package model.category;
  * @author LENOVO
  */
 public class UserObjectDTO {
+    private int userObjectId;
+    private String userObjetctName;
+    private String detail;
+
+    public UserObjectDTO() {
+        userObjectId = 0;
+        userObjetctName = "";
+        detail = "";
+    }
+
+    public UserObjectDTO(int userObjectId, String userObjetctName, String detail) {
+        this.userObjectId = userObjectId;
+        this.userObjetctName = userObjetctName;
+        this.detail = detail;
+    }
+
+    public int getUserObjectId() {
+        return userObjectId;
+    }
+
+    public void setUserObjectId(int userObjectId) {
+        this.userObjectId = userObjectId;
+    }
+
+    public String getUserObjetctName() {
+        return userObjetctName;
+    }
+
+    public void setUserObjetctName(String userObjetctName) {
+        this.userObjetctName = userObjetctName;
+    }
+
+    public String getDetail() {
+        return detail;
+    }
+
+    public void setDetail(String detail) {
+        this.detail = detail;
+    }
+    
+    
     
 }
