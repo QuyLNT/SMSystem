1424717f6e99527eeef9ebde3570fa38471c62e3
KieuMy <MyLTKSE184721@fpt.edu.vn>
2024-10-06 18:10:32 +0700

    CreateProductController

--- SMSystem/src/java/model/product/ProductImageDAO.java
+++ SMSystem/src/java/model/product/ProductImageDAO.java
@@ -5,9 +5,32 @@
  */
 package model.product;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import utils.DBUtils;
+
 /**
  *
  * @author LENOVO
  */
 public class ProductImageDAO {
+    private static final String INSERT_IMAGE_SQL = "INSERT INTO Product_Images (productId, imagePath) VALUES (?, ?)";
+    
+    public void addProductImage(int productId, String image) throws SQLException, ClassNotFoundException {
+        Connection conn = null;
+        PreparedStatement ps = null;
+
+        try {
+            conn = DBUtils.getConnection();
+            ps = conn.prepareStatement(INSERT_IMAGE_SQL);
+            ps.setInt(1, productId);
+            ps.setString(2, image);
+            ps.executeUpdate();
+        } finally {
+            if (ps != null) ps.close();
+            if (conn != null) conn.close();
+        }
+    }
 }
+
