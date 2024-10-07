2b6ee81b913ddb06a27773fea0bd3830888b0a6e
KieuMy <MyLTKSE184721@fpt.edu.vn>
2024-10-07 11:18:17 +0700

    Auto stash before merge of "develop" and "origin/develop"

--- SMSystem/src/java/model/product/ProductDAO.java
+++ SMSystem/src/java/model/product/ProductDAO.java
@@ -123,4 +123,54 @@ public class ProductDAO {
         }
         return productId;
     }
+    public int addProduct(ProductDTO product) throws SQLException, ClassNotFoundException {
+        int productId = -1;
+        PreparedStatement ps = null;
+        ResultSet generatedKeys = null;
+        Connection con = null;
+        String sql = "INSERT INTO Product (productId, brandId, userOjectId, detail, hot, name, color, price, sale, warrantyPeriod, productStatus) "
+                + "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+        try {
+            con = DBUtils.getConnection();
+            ps = con.prepareStatement(sql, PreparedStatement.RETURN_GENERATED_KEYS);
+            ps.setInt(1, product.getProductId());
+            ps.setInt(2, product.getBrandID());
+            ps.setInt(3, product.getUserOjectId());
+            ps.setString(4, product.getDetail());
+            ps.setBoolean(5, product.isHot());
+            ps.setString(6, product.getName());
+            ps.setString(7, product.getColor());
+            ps.setFloat(8, product.getPrice());
+            ps.setFloat(9, product.getSale());
+            ps.setInt(10, product.getWarrantyPeriod());
+            ps.setBoolean(11, product.isProductStaus());
+
+            int rowsInserted = ps.executeUpdate();
+
+            if (rowsInserted > 0) {
+                generatedKeys = ps.getGeneratedKeys();
+                if (generatedKeys.next()) {
+                    productId = generatedKeys.getInt(1);
+                } else {
+                    throw new SQLException("Creating product failed, no ID obtained.");
+                }
+            }
+        } finally {
+            if (generatedKeys != null) {
+                try {
+                    generatedKeys.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (ps != null) {
+                try {
+                    ps.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return productId;
+    }
 }
--- SMSystem/src/java/model/product/ProductImageDAO.java
+++ SMSystem/src/java/model/product/ProductImageDAO.java
@@ -18,6 +18,11 @@ import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import utils.DBUtils;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import utils.DBUtils;
+
 /**
  *
  * @author LENOVO
@@ -54,6 +59,23 @@ public class ProductImageDAO {
         }
         
         return listImage;
+    }
+    private static final String INSERT_IMAGE_SQL = "INSERT INTO Product_Images (productId, imagePath) VALUES (?, ?)";
+    
+    public void addProductImage(int productId, String image) throws SQLException, ClassNotFoundException {
+        Connection conn = null;
+        PreparedStatement ps = null;
+
+        try {
+            conn = DBUtils.getConnection();
+            ps = conn.prepareStatement(INSERT_IMAGE_SQL);
+            ps.setInt(1, productId);
+            ps.setString(2, image);
+            ps.executeUpdate();
+        } finally {
+            if (ps != null) ps.close();
+            if (conn != null) conn.close();
+        }
     }
     private static final String INSERT_IMAGE_SQL = "INSERT INTO Product_Images (productId, imagePath) VALUES (?, ?)";
     
